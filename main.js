1aacf599cf74629f3c3fb3e88fb71762a85f806c
Sajib1472 <132594756+Sajib1472@users.noreply.github.com>
2024-01-28 14:14:41 +0600

    added js

---
+++ main.js
@@ -0,0 +1,33 @@
+// var typed = new Typed(".typing", {
+//     strings: ["Teacher", "Researcher", "Photographer"],
+//     typeSpeed: 100,
+//     backSpeed: 60,
+//     loop: true
+// });
+var typed = new Typed(".typing", {
+    strings: ["Student","Photographer","Researcher"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark_mode");
+ }
+ // function myFunction (iconID){
+ //     var element = document.body;
+ //     element.classList.toggle("dark_mode");
+ //     if( document.getElementById("dark_img").src == "./image/dark_mode_FILL.png"){
+ //         document.getElementById("dark_img").src = "./image/light_mode_FILL.png";
+ //     }
+ //     else if( document.getElementById("dark_img").src == "1.png" ){
+ //         document.getElementById("dark_img").src = "down.png";
+ //     }
+ // }
+
+ // $(document).ready(function(){
+
+
+
+ // });
